72d2f7fd82506c3e0ce2ff9370b39ceee21149ac
Paresh-29 <pareshkumarbarick99@gmail.com>
2024-05-24 22:50:00 +0530

    chore: Update npm dependencies, add new components, and optimize code formatting

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -4,18 +4,22 @@ import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
-    <section id='about'>
-        <BentoGrid>
-            {gridItems.map((item)=> (
-                <BentoGridItem
-                    id={item.id}
-                    key={item.id}
-                    title={item.title}
-                    description={item.description}
-                    className={item.className}
-                />
-            ))}
-        </BentoGrid>
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map(({id, title, description, className, img, imgClassName,titleClassName, spareImg}) => (
+          <BentoGridItem
+            id = {id}
+            key={id}
+            title = {title}
+            description = {description}
+            className={className}
+            img={img}
+            imgClassName={imgClassName}
+            titleClassName={titleClassName}
+            spareImg={spareImg}
+          />
+        ))}
+      </BentoGrid>
     </section>
   )
 }
--- data/index.ts
+++ data/index.ts
@@ -11,7 +11,7 @@ export const navItems = [
       title: "I prioritize client collaboration, fostering open communication ",
       description: "",
       className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-      imgClassName: "w-full h-full",
+      imgClassName: "w-auto h-auto",
       titleClassName: "justify-end",
       img: "/b1.svg",
       spareImg: "",
